a40dda675bb4c3ffe530bde800a19a1e69257b5e
shahriar eshraghi <shahriareshraghi22@gmail.com>
2022-02-09 23:07:28 +0330

    add about component and page

---
+++ pages/aboutus/index.js
@@ -0,0 +1,13 @@
+import {Fragment} from "react";
+import About from "../../components/about/about";
+
+const AboutPage = () => {
+
+    return (
+        <Fragment>
+            <About/>
+        </Fragment>
+    )
+};
+
+export default AboutPage;
