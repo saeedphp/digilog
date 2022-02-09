7fa02d1fcfe53d1e7e75f608143de6ffbc625a5b
shahriar eshraghi <shahriareshraghi22@gmail.com>
2022-02-09 22:45:16 +0330

    add header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import "../public/font/fontawesome-pro-5.15.1-web-ulabs/css/all.min.css";
+import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  )
 }
 
 export default MyApp
