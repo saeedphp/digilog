93c1b9fc1eb5c06e6cab6c5939cc708c3c557dad
shahriar eshraghi <shahriareshraghi22@gmail.com>
2022-02-09 23:00:29 +0330

    add index, process component and sections

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
 import {Fragment} from "react";
 import Hero from "../components/hero/hero";
+import IndexAbout from "../components/index-about/index-about";
+import Process from "../components/process/process";
+import {getAllProcessItems} from "../data/process";
 
 const HomePage = () => {
+
+    const processItems = getAllProcessItems();
+
   return (
       <Fragment>
           <Hero />
+          <IndexAbout />
+          <div className={`txt-center how-wrapper`}>
+              <h2 className={`howTxt`}>
+                  چه جوری با دیجی لاگ بوک
+              </h2>
+              <p className={`green-txt fly`}>
+                  پروازهامون
+              </p>
+              <br/>
+              <span>
+                    رو
+                </span>
+              <span className={`green-txt`}>
+                    بخاطر
+                </span>
+              <span>
+                    بسپاریم
+                </span>
+          </div>
+          <Process processItems={processItems} />
       </Fragment>
   )
 };
